ae9ce6d442bd682ea81c476e86b2fb0c44182559
Tekkem Venkatesh <tekkem.venkatesh@mindtree.com>
2020-03-13 14:43:33 +0530

    Step7: create quiz component.

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,7 +20,7 @@ class Quiz extends Component {
    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
     return (
       <div>
-        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>
     )
   }
